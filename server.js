bc8a601db62b0b99bba76419ab8239f6a07ea31f
Jeremy Coathup <jeremycoathup@gmail.com>
2018-06-27 21:08:00 +0100

    added Twit module and testing modified server.js file

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
-var express = require('express');
-var app = express();
+var fs = require('fs'),
+    path = require('path'),
+    Twit = require('twit'),
+    config = require(path.join(__dirname, 'config.js')),
+    express = require('express'),
+    app = express();
 app.use(express.static(__dirname + '/'));
 app.get('/', function (req, res){
   res.render('index.html', {})
---
+++ twitter_config.js
@@ -0,0 +1,8 @@
+var config = {
+  consumer_key:         'H60gv0eNf25CxyMB5LhH5ngkz',
+  consumer_secret:      'RC48SDWeNROqI9zmNpkETml55KACCGwghAK7K9RyxjefjCwtPI',
+  access_token:         '1012061241848860673-ad9VlrFMpcF3GODCiLsXMhJl8Yk003',
+  access_token_secret:  'e2OnA9hUXR9VW1sTNxME0z3rvHazvp6EQXP7kvh5PAdST'
+}
+
+module.exports = config;

5012bd5b72cb7e41a8193cfc8a577ba45e499b46
Jeremy Coathup <jeremycoathup@gmail.com>
2018-07-27 13:06:11 +0100

    fixing canvas blur

--- scripts.js
+++ scripts.js
@@ -313,7 +313,7 @@ function resizeCanvas(){
 
 //context.imageSmoothingEnabled = true; /// future
 
-  context.drawImage(tempCanvas, 0, 0, tempCanvas.width, tempCanvas.height, 0, 0, canvas.width*dpi, canvas.height*dpi);
+  context.drawImage(tempCanvas, 0, 0, tempCanvas.width*dpi, tempCanvas.height*dpi, 0, 0, canvas.width*dpi, canvas.height*dpi);
 
   //context.scale(canvas.width/tempCanvas.width, canvas.height/tempCanvas.height);
   context.fillStyle = ink;

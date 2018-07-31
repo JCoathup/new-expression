e02461e2d26e3fa97d38c9d06ee94ee16968b2ae
Jeremy Coathup <jeremycoathup@gmail.com>
2018-07-31 16:07:06 +0100

    test

--- scripts.js
+++ scripts.js
@@ -315,7 +315,7 @@ function resizeCanvas(){
 //context.imageSmoothingEnabled = true; /// future
 
   context.drawImage(tempCanvas, 0, 0, tempCanvas.width, tempCanvas.height, 0, 0, canvas.width, canvas.height);
-canvas.scale(tempCanvas.width/window.innerWidth, tempCanvas.height/window.innerHeight);
+//canvas.scale(tempCanvas.width/window.innerWidth, tempCanvas.height/window.innerHeight);
   //context.scale(canvas.width/tempCanvas.width, canvas.height/tempCanvas.height);
   context.fillStyle = ink;
   context.strokeStyle = ink;

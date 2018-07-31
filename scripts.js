14155eba2d2bfb371324b926abbc191dce27704d
Jeremy Coathup <jeremycoathup@gmail.com>
2018-07-31 21:32:43 +0100

    test for screen slip

--- scripts.js
+++ scripts.js
@@ -173,6 +173,7 @@ document.addEventListener("touchmove", function(e){
     if (!e)
         var e = event;
     if (e.touches) {
+      e.preventDefault();
         if (e.touches.length == 1) { // Only deal with one finger
             var touch = e.touches[0]; // Get the information for finger #1
             touchX=touch.pageX-touch.target.offsetLeft;

618340b226739905985a4eb4e1c26567ae5bd3ad
Jeremy Coathup <jeremycoathup@gmail.com>
2018-08-04 21:13:30 +0100

    resize functionality

--- scripts.js
+++ scripts.js
@@ -341,7 +341,7 @@ console.log(img.width, img.height);
   context.drawImage(img, 0, 0);
 //canvas.scale(tempCanvas.width/window.innerWidth, tempCanvas.height/window.innerHeight);
   //context.scale(canvas.width/tempCanvas.width, canvas.height/tempCanvas.height);
-  context.scale(1,1);
+  context.scale(canvas.width, canvas.height);
 }
   context.fillStyle = ink;
   context.strokeStyle = ink;

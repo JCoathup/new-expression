27bcbc17c7e167c71f1e908ddced9adffe95a678
Jeremy Coathup <jeremycoathup@gmail.com>
2018-08-05 02:57:36 +0100

    resize functionality

--- scripts.js
+++ scripts.js
@@ -335,7 +335,7 @@ img.onload = function() {
 context.globalCompositeOperation = "source-over";
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-context.drawImage(img, 0, 0, tempCanvas.width, tempCanvas.height, 0, 0, canvas.width/tempCanvas.width, canvas.height/tempCanvas.height);
+context.drawImage(img, 0, 0, tempCanvas.width, tempCanvas.height, 0, 0, canvas.width, canvas.height);
 //canvas.scale(tempCanvas.width/window.innerWidth, tempCanvas.height/window.innerHeight);
   //context.scale(canvas.width/tempCanvas.width, canvas.height/tempCanvas.height);
   context.scale(canvas.width/tempCanvas.width, canvas.height/tempCanvas.height);

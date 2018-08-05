a2a4da4d10beb6ccd47aa809d9a8081f8e4412c2
Jeremy Coathup <jeremycoathup@gmail.com>
2018-08-05 01:12:29 +0100

    resize functionality

--- scripts.js
+++ scripts.js
@@ -335,7 +335,7 @@ img.onload = function() {
 //context.imageSmoothingEnabled = true; /// future
 let context = canvas.getContext("2d");
 context.globalCompositeOperation = "source-over";
-context.drawImage(img, 0, 0, canvas.width, canvas.height, 0, 0, canvas.width/tempCanvas.width, canvas.height/tempCanvas.height);
+context.drawImage(img, 0, 0, tempCanvas.width, tempCanvas.height, 0, 0, canvas.width/tempCanvas.width, canvas.height/tempCanvas.height);
 //canvas.scale(tempCanvas.width/window.innerWidth, tempCanvas.height/window.innerHeight);
   //context.scale(canvas.width/tempCanvas.width, canvas.height/tempCanvas.height);
   //ctx.scale(widthy/img.width, heighty/img.height);

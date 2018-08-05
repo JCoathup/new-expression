734988bab2662cd13855ac1519e16d57f7a633c2
Jeremy Coathup <jeremycoathup@gmail.com>
2018-08-05 10:16:32 +0100

    resize functionality

--- scripts.js
+++ scripts.js
@@ -344,7 +344,7 @@ canvas.style.width = "100%";
 canvas.style.width = "100%";
 context.canvas.width = window.innerWidth;
 context.canvas.height = window.innerHeight;
-context.drawImage(img, 0, 0, tempCanvas.width, tempCanvas.height, 0, 0, canvas.width/tempCanvas.width, canvas.width/tmpCanvas.height);
+context.drawImage(img, 0, 0, tempCanvas.width, tempCanvas.height, 0, 0, canvas.width/tempCanvas.width, canvas.width/tempCanvas.height);
 //canvas.scale(tempCanvas.width/window.innerWidth, tempCanvas.height/window.innerHeight);
   //context.scale(canvas.width/tempCanvas.width, canvas.height/tempCanvas.height);
   context.scale(canvas.width/tempCanvas.width, canvas.height/tempCanvas.height);

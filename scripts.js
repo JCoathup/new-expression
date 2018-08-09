3f4ab61acee69e5a3fbfde26e4e6d3b70a506dad
Jeremy Coathup <jeremycoathup@gmail.com>
2018-08-09 12:16:29 +0100

    extending browser window

--- scripts.js
+++ scripts.js
@@ -314,8 +314,26 @@ function erase (){
  	//return the Base64 encoded data url string
  	return imageData;
  }
-window.addEventListener("resize", resizeCanvas, false);
+window.addEventListener("resize", resizingCanvas, false);
 //window.addEventListener("orientationchange", resizeCanvas, false);
+
+function resizingCanvas(){
+  /*let main = document.getElementById("main");
+  main.style.backgroundColor = "black";*/
+  let ink = context.fillStyle;
+  let bgd = canvas.style.backgroundColor;
+  let img = new Image();
+  img.src = canvasToImage(bgd);
+  img.onload = function() {
+    scaleToFill(this);
+  }
+  function scaleToFill(){
+    let scale = Math.max(canvas.width / img.width, canvas.height / img.height);
+    let x = (canvas.width / 2) - (img.width / 2) * scale;
+    let y = (canvas.height / 2) - (img.height / 2) * scale;
+    context.drawImage(img, x, y, img.width * scale, img.height * scale);
+  }
+}
 
 function resizeCanvas(){
   /*let main = document.getElementById("main");

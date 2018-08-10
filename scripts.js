63414bb78b10bb703bdc443e7027be370b6dcad8
Jeremy Coathup <jeremycoathup@gmail.com>
2018-08-10 14:37:48 +0100

    extending browser window

--- scripts.js
+++ scripts.js
@@ -326,15 +326,16 @@ function resizingCanvas(){
   let bgd = canvas.style.backgroundColor;
   let img = new Image();
   img.src = canvasToImage(bgd);
-    let scale = Math.min(canvas.width / img.width, canvas.height / img.height);
-  let x = (canvas.width / 2) - (img.width / 2) * scale;
-  let y = (canvas.height / 2) - (img.height / 2) * scale;
+
   canvas.setAttribute("width", window.innerWidth);
   canvas.setAttribute("height", window.innerHeight);
   img.onload = function() {
     scaleToFill(this);
   }
   function scaleToFill(img){
+    let scale = Math.min(canvas.width / img.width, canvas.height / img.height);
+  let x = (canvas.width / 2) - (img.width / 2) * scale;
+  let y = (canvas.height / 2) - (img.height / 2) * scale;
     if (img.width >= canvas.width || img.height >= canvas.height){
       console.log("bigger image");
       context.drawImage(img, x,y, img.width, img.height);

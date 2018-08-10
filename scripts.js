abf91bdd32ebac0e8868d9d9c14e9c104e0cc9c2
Jeremy Coathup <jeremycoathup@gmail.com>
2018-08-10 14:42:50 +0100

    extending browser window

--- scripts.js
+++ scripts.js
@@ -338,6 +338,7 @@ function resizingCanvas(){
   let y = (canvas.height / 2) - (img.height / 2) * scale;
     if (img.width >= canvas.width || img.height >= canvas.height){
       console.log("bigger image");
+      let scale = Math.max(canvas.width / img.width, canvas.height / img.height);
       context.drawImage(img, x,y, img.width*scale, img.height*scale);
       context.fillStyle = ink;
       context.strokeStyle = ink;

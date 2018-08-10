265225eb24edce68a8def253208fa54848b8f39e
Jeremy Coathup <jeremycoathup@gmail.com>
2018-08-10 14:36:21 +0100

    extending browser window

--- scripts.js
+++ scripts.js
@@ -326,7 +326,7 @@ function resizingCanvas(){
   let bgd = canvas.style.backgroundColor;
   let img = new Image();
   img.src = canvasToImage(bgd);
-    let scale = Math.max(canvas.width / img.width, canvas.height / img.height);
+    let scale = Math.min(canvas.width / img.width, canvas.height / img.height);
   let x = (canvas.width / 2) - (img.width / 2) * scale;
   let y = (canvas.height / 2) - (img.height / 2) * scale;
   canvas.setAttribute("width", window.innerWidth);

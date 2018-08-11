69c2ce452296624f93b3ffa79b70007637ee2eff
Jeremy Coathup <jeremycoathup@gmail.com>
2018-08-11 10:46:18 +0100

    extending browser window

--- scripts.js
+++ scripts.js
@@ -362,6 +362,7 @@ let y = (canvas.height / 2) - (tempCanvas.height / 2) * scale;
  canvas.setAttribute("height", window.innerHeight);
  if (tempCanvas.width > canvas.width || tempCanvas.height > canvas.height){
    console.log("BIGGER");
+   canvas.getContext('2d').drawImage(tempCanvas, x, y, tempCanvas.width, tempCanvas.height);
    return;
  }
  console.log("wont see this");

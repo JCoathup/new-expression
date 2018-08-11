1d64f49599350fc86fc3c0a8e6adf3aac0031d31
Jeremy Coathup <jeremycoathup@gmail.com>
2018-08-11 15:41:05 +0100

    extending browser window

--- scripts.js
+++ scripts.js
@@ -363,7 +363,7 @@ let tempCanvas = document.createElement('canvas');
      canvas.setAttribute("height", window.innerHeight);
      if (tempCanvas.width > canvas.width || tempCanvas.height > canvas.height){
        console.log("BIGGER");
-       let scale = Math.max(tempCanvas.width / canvas.width, tempCanvas.height / canvas.height);
+       let scale = Math.min(tempCanvas.width / canvas.width, tempCanvas.height / canvas.height);
        context.drawImage(tempCanvas, 0, 0);
        context.fillStyle = ink;
        context.strokeStyle = ink;

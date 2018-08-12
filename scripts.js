f8aaa7d770b9d251e6590d46f6fa47f0535ff996
Jeremy Coathup <jeremycoathup@gmail.com>
2018-08-12 01:08:19 +0100

    more window downsizing

--- scripts.js
+++ scripts.js
@@ -362,6 +362,8 @@ let tempCanvas = document.createElement('canvas');
      canvas.setAttribute("width", window.innerWidth);
      canvas.setAttribute("height", window.innerHeight);
      if (tempCanvas.width > canvas.width || tempCanvas.height > canvas.height){
+       canvas.setAttribute("width", tempCanvas.width);
+       canvas.setAttribute("height", tempCanvas.height);
        console.log("BIGGER");
        let scale = Math.min(tempCanvas.width / canvas.width, tempCanvas.height / canvas.height);
        context.drawImage(tempCanvas, 0, 0, tempCanvas.width, tempCanvas.height);

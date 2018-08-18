12d8960d08c8f814d4f82db1dae0bc19604211d2
Jeremy Coathup <jeremycoathup@gmail.com>
2018-08-18 22:01:21 +0100

    mobile orientation

--- scripts.js
+++ scripts.js
@@ -385,7 +385,7 @@ function Orientationshift(){
   let img = new Image();
   img.src = canvasToImage(bgd);
   img.onload = function() {
-    ctx.drawImage(image,canvas.width/2-image.width/2,canvas.height/2-image.width/2);
+    ctx.drawImage(img,canvas.width/2-image.width/2,canvas.height/2-image.width/2);
   }
   tempContext.drawImage(canvas, 0, 0);
   switch(window.orientation){

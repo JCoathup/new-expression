3917a385da5e26f5027d018d428761cf6f8618ee
Jeremy Coathup <jeremycoathup@gmail.com>
2018-08-18 10:37:56 +0100

    mobile orientation fix

--- scripts.js
+++ scripts.js
@@ -400,6 +400,7 @@ tempCanvas.getContext('2d').drawImage(canvas, 0, 0);
 
      // Copy back to resized canvas
      context = canvas.getContext('2d');
+     context.rotate(90*Math.PI/180);
      context.drawImage(tempCanvas, 0, 0, tempCanvas.width, tempCanvas.height, 0, 0, canvas.width, canvas.height);
    break;
    default:

3e66e19470d69b428db0957567e99215fb352d61
Jeremy Coathup <jeremycoathup@gmail.com>
2018-08-20 14:54:23 +0100

    mobile orientation

--- scripts.js
+++ scripts.js
@@ -394,7 +394,7 @@ function Orientationshift(){
     case 90:
     angleInDegrees+=90;
     //drawRotated(angleInDegrees);
-    canvas.classList.add("canvasRotated90");
+    //canvas.classList.add("canvasRotated90");
     context.save();
     context.translate(canvas.width/2, canvas.height/2);
     context.rotate(90*Math.PI/180);

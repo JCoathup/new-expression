fdbc388678469fc3f48484ff1346b9137d3e2fed
Jeremy Coathup <jeremycoathup@gmail.com>
2018-08-20 09:57:12 +0100

    mobile orientation

--- scripts.js
+++ scripts.js
@@ -372,7 +372,7 @@ let tempCanvas = document.createElement('canvas');
        return;
      }
      console.log("SMALLER");
-     canvas.getContext('2d').drawImage(tempCanvas, 0, 0, tempCanvas.width*scale, tempCanvas.height*scale);
+     canvas.getContext('2d').drawImage(tempCanvas, 0, 0, tempCanvas.width, tempCanvas.height);
 context.fillStyle = ink;
  context.strokeStyle = ink;
 }

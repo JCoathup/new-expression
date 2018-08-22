5d94ecd037ae36009507bb8a827e0057f2f92ef5
Jeremy Coathup <jeremycoathup@gmail.com>
2018-08-22 13:25:08 +0100

    mobile orientation

--- scripts.js
+++ scripts.js
@@ -417,8 +417,8 @@ function drawRotated(degrees){
     console.log(degrees);
     context.clearRect(0,0,canvas.width,canvas.height);
     context.save();
-    canvas.setAttribute("width", screen.height);
-    canvas.setAttribute("height", screen.width);
+    canvas.setAttribute("width", window.innerHeight);
+    canvas.setAttribute("height", window.innerWidth);
     context.translate(canvas.width/2,canvas.height/2);
     context.rotate(degrees*Math.PI/180);
     context.drawImage(tempCanvas,-tempCanvas.width/2,-tempCanvas.height/2);

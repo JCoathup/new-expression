63d4fa413be7e48e56ef9dd73779001d067fc070
Jeremy Coathup <jeremycoathup@gmail.com>
2018-08-24 08:12:56 +0100

    window orientation

--- scripts.js
+++ scripts.js
@@ -330,8 +330,8 @@ function canvasResize(){
      canvas.setAttribute("width", window.innerWidth);
      canvas.setAttribute("height", window.innerHeight);
      if (tempCanvas.width > canvas.width || tempCanvas.height > canvas.height){
-       canvas.setAttribute("width", tempCanvas.width);
-       canvas.setAttribute("height", tempCanvas.height);
+       canvas.setAttribute("width", tempCanvas.width*window.devicePixelRatio);
+       canvas.setAttribute("height", tempCanvas.height*window.devicePixelRatio);
        console.log("BIGGER");
        let scale = Math.max(tempCanvas.width / canvas.width, tempCanvas.height / canvas.height);
        canvas.getContext('2d').drawImage(tempCanvas, 0, 0, tempCanvas.width, tempCanvas.height);

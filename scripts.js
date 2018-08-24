c6341371665d30917c967bfc4ae17f3f5ec80e5b
Jeremy Coathup <jeremycoathup@gmail.com>
2018-08-24 21:20:19 +0100

    window orientation

--- scripts.js
+++ scripts.js
@@ -397,7 +397,7 @@ function drawRotated(degrees){
     console.log(degrees);
     context.clearRect(0,0,canvas.width,canvas.height);
     context.save();
-    canvas.setAttribute("width", window.innerHeight*window.devicePixelRatio);
+    canvas.setAttribute("width", window.innerHeight);
     canvas.setAttribute("height", window.innerWidth);
     context.translate(canvas.width/2,canvas.height/2);
     context.rotate(degrees*Math.PI/180);

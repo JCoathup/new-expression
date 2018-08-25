9a46abddff1f786c9f7c7cafa3f2cd89a67aca29
Jeremy Coathup <jeremycoathup@gmail.com>
2018-08-25 15:34:23 +0100

    window orientation

--- scripts.js
+++ scripts.js
@@ -358,14 +358,20 @@ function Orientationshift(){
 
   switch(window.orientation){
     case -90:
-    angleInDegrees+=90;
-    drawRotated(angleInDegrees);
-    rotation = 1;
+    if (rotation == 1){
+      angleInDegrees+=180;
+      drawRotated180(angleInDegrees);
+    }
+    else{
+      angleInDegrees+=90;
+      drawRotated(angleInDegrees);
+      rotation = 1;
+    }
     break;
     case 90:
     if (rotation == 1){
       angleInDegrees-=180;
-      drawRotated(angleInDegrees);
+      drawRotated180(angleInDegrees);
     }
     else{
       angleInDegrees-=90;
@@ -391,13 +397,25 @@ function Orientationshift(){
 }
 
 function drawRotated(degrees){
+    console.log(degrees);
     context.clearRect(0,0,canvas.width,canvas.height);
     context.save();
     canvas.setAttribute("width", window.innerHeight*window.devicePixelRatio);
     canvas.setAttribute("height", window.innerWidth);
     context.translate(canvas.width/2,canvas.height/2);
     context.rotate(degrees*Math.PI/180);
-    //context.scale(window.innerWidth/canvas.width, window.innerHTML/canvas.height);
+    context.drawImage(tempCanvas,-tempCanvas.width/2,-tempCanvas.height/2);
+    context.restore();
+}
+
+function drawRotated180(degrees){
+    console.log(degrees);
+    context.clearRect(0,0,canvas.width,canvas.height);
+    context.save();
+    canvas.setAttribute("width", window.innerHeight*window.devicePixelRatio);
+    canvas.setAttribute("height", window.innerWidth);
+    context.translate(-canvas.width/2,-canvas.height/2);
+    context.rotate(degrees*Math.PI/180);
     context.drawImage(tempCanvas,-tempCanvas.width/2,-tempCanvas.height/2);
     context.restore();
 }

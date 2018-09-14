453a21f457f67a3bf7767a54f39be1cde30e303c
Jeremy Coathup <jeremycoathup@gmail.com>
2018-09-14 20:03:44 +0100

    resize and erase

--- scripts.js
+++ scripts.js
@@ -310,7 +310,7 @@ function clrscreen(){
 //erase functionality
 function erase (){
   let eraseButton = document.querySelector(".eraseButton");
-  if (eraseButton.innerHTML == " "){
+  if (document.querySelector("#endErase") == "null"){
     eraseButton.innerHTML += `<button id="endErase">Stop Erasing</button>`;
   }
   lastColour = context.strokeStyle;

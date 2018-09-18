e922ad30a1e45ad1fb3e341831c3fc8f459f4084
Jeremy Coathup <jeremycoathup@gmail.com>
2018-09-18 12:24:19 +0100

    twit test

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ io.sockets.on('connection', function(socket) {
   //on user disconnections
   socket.on('dispatch', function(data){
 
-  /*  T.get('account/verify_credentials', { skip_status: true })
+   T.get('account/verify_credentials', { skip_status: true })
   .catch(function (err) {
     console.log('caught error', err.stack)
   })
@@ -40,56 +40,7 @@ io.sockets.on('connection', function(socket) {
 
     console.log('data', result.data);
     console.log('response', result.resp);
-  }) */
-
-  var oa = new OAuth(
-    "https://api.twitter.com/oauth/request_token",
-    "https://api.twitter.com/oauth/access_token",
-    "CQxtt9hErmWZDuu4Rsya3WgzA",
-    "LUtXBVADsJP7T9YXDEpJS5lWQT7tKJJ6wz60Az7rGDPfQkDKnb",
-    "1.0",
-    "https://new-expression.herokuapp.com",
-    "HMAC-SHA1"
-  );
-
-  app.get('/', function(req, res){
-	oa.getOAuthRequestToken(function(error, oauth_token, oauth_token_secret, results){
-		if (error) {
-			console.log(error);
-			res.send("yeah no. didn't work.")
-		}
-		else {
-			req.session.oauth = {};
-			req.session.oauth.token = oauth_token;
-			console.log('oauth.token: ' + req.session.oauth.token);
-			req.session.oauth.token_secret = oauth_token_secret;
-			console.log('oauth.token_secret: ' + req.session.oauth.token_secret);
-			res.redirect('https://twitter.com/oauth/authenticate?oauth_token='+oauth_token)
-	}
-	});
-});
-
-app.get('/', function(req, res, next){
-	if (req.session.oauth) {
-		req.session.oauth.verifier = req.query.oauth_verifier;
-		var oauth = req.session.oauth;
-
-		oa.getOAuthAccessToken(oauth.token,oauth.token_secret,oauth.verifier,
-		function(error, oauth_access_token, oauth_access_token_secret, results){
-			if (error){
-				console.log(error);
-				res.send("yeah something broke.");
-			} else {
-				req.session.oauth.access_token = oauth_access_token;
-				req.session.oauth,access_token_secret = oauth_access_token_secret;
-				console.log(results);
-				res.send("worked. nice one.");
-			}
-		}
-		);
-	} else
-		next(new Error("you're not supposed to be here."))
-});
+  })
 
     console.log(socket.id, "tweeted", data.tweetContent);
     var message = data.tweetContent;

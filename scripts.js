4f3ebdd21438a52c3d4aec9c5f0803e5657557d0
Jeremy Coathup <jeremycoathup@gmail.com>
2018-09-20 16:55:04 +0100

    test FB

--- scripts.js
+++ scripts.js
@@ -548,7 +548,9 @@ function sendingTweet () {
 
 function uploadFacebook (data){
   FB.ui({
+    display: 'popup',
   method: 'share',
+  href: 'https://new-expression.herokuapp.com/'+data,
   url: 'https://new-expression.herokuapp.com',
   action_type: 'og.shares',
   action_properties: JSON.stringify({

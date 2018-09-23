238edf1f6994bbfdd0e44bc4e9ac38dce0a42c97
Jeremy Coathup <jeremycoathup@gmail.com>
2018-09-23 22:48:41 +0100

    twitter oauth

--- scripts.js
+++ scripts.js
@@ -547,7 +547,7 @@ function sendingTweet () {
 }
 
 function uploadFacebook (data){
-/*  FB.ui({
+ FB.ui({
   method: 'share_open_graph',
   href: 'https://new-expression.herokuapp.com/',
   url: 'https://new-expression.herokuapp.com',
@@ -567,14 +567,13 @@ function uploadFacebook (data){
   })
 }, function(response){
   console.log(response);
-}); */
-FB.ui(
+});
+/* FB.ui(
   {
     method: 'feed',
     name: 'Scribblez',
     href: 'https://new-expression.herokuapp.com',
     picture: "https://new-expression.herokuapp.com/" + data
   }
-);
-con
+);*/
 }

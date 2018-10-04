05b6fe173e80a751a2f5380de469db63e8e1cca5
Jeremy Coathup <jeremycoathup@gmail.com>
2018-10-04 18:58:27 +0100

    gj

--- server.js
+++ server.js
@@ -90,7 +90,7 @@ app.get('/twitter', passport.authenticate('twitter'),
   app.get('/twitter/callback', passport.authenticate("twitter"), function(req, res){
     res.send("you reached the callback uri");
     res.writeHead(302, {
-  'Location': '/twitter/tweet'
+  'Location': 'https://new-expression.herokuapp.com/twitter/tweet'
 });
   });
   app.get('/twitter/tweet', function(req, res){

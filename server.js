98de69db0cefc5623c20330ac5125e4a211abc56
Jeremy Coathup <jeremycoathup@gmail.com>
2018-10-04 18:53:46 +0100

    gj

--- server.js
+++ server.js
@@ -89,6 +89,9 @@ app.get('/twitter', passport.authenticate('twitter'),
 
   app.get('/twitter/callback', passport.authenticate("twitter"), function(req, res){
     res.send("you reached the callback uri");
+    response.writeHead(302, {
+  'Location': '/twitter/tweet'
+});
   });
   app.get('/twitter/tweet', function(req, res){
     postTweet(function(error, data) {

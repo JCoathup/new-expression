f4f4be21035633e5e1005f795374ddb554901022
Jeremy Coathup <jeremycoathup@gmail.com>
2018-10-05 11:32:29 +0100

    twitter

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ function postTweet(callbacker){
     , user.token
     , user.tokenSecret
     // We just have a hard-coded tweet for now
-    , { "media_data": twitterImage}
+    , { "media_data": twitterImage, "status": "yippeeeee"}
     , callbacker
     );
 }

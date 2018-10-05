0f53eb34e3c4fc8a759d1091ff048e4de493efc5
Jeremy Coathup <jeremycoathup@gmail.com>
2018-10-05 21:12:12 +0100

    twitter

--- twitter_config.js
+++ twitter_config.js
@@ -1,8 +1,8 @@
 var config = {
   consumer_key:         'CQxtt9hErmWZDuu4Rsya3WgzA',
   consumer_secret:      'LUtXBVADsJP7T9YXDEpJS5lWQT7tKJJ6wz60Az7rGDPfQkDKnb',
-  access_token:         '28579391-5MX2k18y2QePrgPLPdroW2NAkdFhAkEuDYRTSIhgn',
-  access_token_secret:  'Lg5ZIYhjUjukMNCOEQad4L2HAgocb9kl2Q8Wyk0KebZYH',
+  access_token:         '28579391-Eii2FKSn0ynKd2q8WCeIB7To4rAXo3zOYQib3PJ03',
+  access_token_secret:  'NwNr1PuE7Y2WHSd215RrYRdlmCVHBIjMpxUnLV2uho7Nn',
   callback_url: 'https://new-expression.herokuapp.com/twitter/callback'
 }
 

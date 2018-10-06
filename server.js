a49f77bd231ae7013a83db3ba5d4876f40a3e433
Jeremy Coathup <jeremycoathup@gmail.com>
2018-10-06 13:05:17 +0100

    twitter

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ function postTweet(callbacker){
     //, {"status": "trying", "media_id": twitterImage, "media_id_string": twitterImage}
     ,function(error, data, response){
       console.log("start uploading here. MEDIA DATA: "+  data);
-      socket.emit("messagetype", data);
+      io.emit("messagetype", data);
       data = JSON.parse(data);
       cb(data.media_id_string);
     }

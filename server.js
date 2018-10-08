e5c71ae77d1c2718f3547f6f0d31b5c183d0366e
Jeremy Coathup <jeremycoathup@gmail.com>
2018-10-08 16:57:55 +0100

    tweet

--- server.js
+++ server.js
@@ -118,7 +118,7 @@ function postTweet(callbacker){
       , user.token
       , user.tokenSecret
       // We just have a hard-coded tweet for now
-      , { "status": "testing" }
+      , { "status": "testing!" }
       , function(err, data, response){
         if (err){
           console.log("ERROR:", err);
